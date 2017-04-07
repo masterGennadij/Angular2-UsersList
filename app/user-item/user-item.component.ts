eb4a4cc06fc2ec7a79e7df2a8fe29a44e6c940b2
Alexander Makeew <alex.makeev@genovawebart.com>
2017-04-07 13:53:55 +0300

    add tooltips

--- app/user-item/user-item.component.ts
+++ app/user-item/user-item.component.ts
@@ -12,12 +12,14 @@ export class UserItemComponent{
     @Output() hideAll = new EventEmitter();
     active: boolean = false;
     toggle: boolean = false;
-     @Input() user: any;
+    @Input() user: any;
      @Input() value: boolean;
+
      showDetails(){      
         this.active = !this.active;
         this.toggle = !this.toggle;
      }
+
      hide() {
         this.hideAll.emit(this.user);
     }
